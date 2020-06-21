63fe1a7d8b9a28f49b9a2c90849fcc5e2d52a3de
Nikos Koukis <nickkouk@gmail.com>
2020-06-21 12:52:13 +0100

    Finish first working version of fast features + minimal example

--- examples/detect.rs
+++ examples/detect.rs
@@ -1,22 +1,16 @@
 use cv_detectors::{FASTDetector, ImgCoords, KeypointDetector};
-use image::{open, GrayImage};
-use rand::random;
+use image::open;
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::module_path;
-use std::path::Path;
 use std::process::Command;
 use std::vec::Vec;
 
-pub fn main() {
-    let detector = FASTDetector::default();
+#[macro_use]
+extern crate timeit;
 
-    // let image = GrayImage::from_fn(64, 64, |x, y| {
-    //     let num: u8 = random();
-    //     image::Luma([num; 1])
-    // });
+pub fn main() {
+    let detector_nonmax = FASTDetector::default();
+    let mut detector = FASTDetector::default();
+    detector.params.do_nonmax_suppression = false;
 
     // load image from disk
     let path_to_image = std::env::current_exe()
@@ -25,30 +19,67 @@ pub fn main() {
         .unwrap()
         .join("../../../tests/data/much-smart-very-science-wow.jpg");
     let image_name = path_to_image.to_str().unwrap();
-    let mut image =
-        open(path_to_image.clone()).expect(&format!("Image {} not found", path_to_image.display()));
+    let image = open(path_to_image.clone())
+        .unwrap_or_else(|err| panic!("Couldn't open image {} / {}", path_to_image.display(), err));
     let image_grey = image.clone().into_luma();
 
-    // detect features
+    // output images
+    let mut image_rgb_nonmax = image.clone().into_rgb();
+    let mut image_rgb = image.into_rgb();
+
+    // detect features - nonmax
+    let mut features_nonmax = Vec::<ImgCoords>::new();
+    timeit!({detector_nonmax.detect(&image_grey, &mut features_nonmax)});
+
+    // detect faetures
     let mut features = Vec::<ImgCoords>::new();
-    detector.detect(&image_grey, &mut features);
+    timeit!({detector.detect(&image_grey, &mut features)});
 
-    // draw features over image
-    // TODO
+    // draw features over image - nonmax
+    for feat in features_nonmax.iter() {
+        for neighbor in [[-1i64, 0], [0, 1], [1, 0], [0, -1]]
+            .iter()
+            .map(|p| (p[0] + feat.x as i64, p[1] + feat.y as i64))
+        {
+            let pixel = image_rgb_nonmax.get_pixel_mut(neighbor.0 as u32, neighbor.1 as u32);
+            *pixel = image::Rgb([0, 255, 0]);
+        }
+    }
+    for feat in features.iter() {
+        for neighbor in [[-1i64, 0], [0, 1], [1, 0], [0, -1]]
+            .iter()
+            .map(|p| (p[0] + feat.x as i64, p[1] + feat.y as i64))
+        {
+            let pixel = image_rgb.get_pixel_mut(neighbor.0 as u32, neighbor.1 as u32);
+            *pixel = image::Rgb([0, 255, 0]);
+        }
+    }
 
+    let new_image_name_nonmax = "image_with_features_nonmax.jpg";
     let new_image_name = "image_with_features.jpg";
-    image.save(env::current_dir().unwrap().join(new_image_name));
+    image_rgb_nonmax
+        .save(env::current_dir().unwrap().join(new_image_name_nonmax))
+        .unwrap_or_else(|err| panic!("Couldn't save image {} / {}", new_image_name, err));
+    image_rgb
+        .save(env::current_dir().unwrap().join(new_image_name))
+        .unwrap_or_else(|err| panic!("Couldn't save image {} / {}", new_image_name, err));
 
     // open newly saved image
     let children: Vec<std::process::Child> = vec![
         Command::new("xdg-open")
             .arg(image_name)
             .spawn()
-            .expect(&format!("Unable to open {} image!", image_name)),
+            .unwrap_or_else(|err| panic!("Unable to open {} image / {}", image_name, err)),
+        Command::new("xdg-open")
+            .arg(new_image_name_nonmax)
+            .spawn()
+            .unwrap_or_else(|err| {
+                panic!("Unable to open {} image / {}", new_image_name_nonmax, err)
+            }),
         Command::new("xdg-open")
             .arg(new_image_name)
             .spawn()
-            .expect(&format!("Unable to open {} image!", new_image_name)),
+            .unwrap_or_else(|err| panic!("Unable to open {} image / {}", new_image_name, err)),
     ];
 
     for child in children {
--- src/fast.rs
+++ src/fast.rs
@@ -5,6 +5,7 @@
 // FASTDetector -----------------------------------------------------------------------------------
 
 use image::GrayImage;
+use std::cmp;
 use std::vec::Vec;
 
 use crate::traits::KeypointDetector;
@@ -26,6 +27,25 @@ enum ComparedToCentre {
 }
 
 impl FASTDetector {
+    const NEIGHBOR_RELATIVE_COORDS: [(i8, i8); 16] = [
+        (0, -3),  // 1
+        (1, -3),  // 2
+        (2, -2),  // 3
+        (3, -1),  // 4
+        (3, 0),   // 5
+        (3, 1),   // 6
+        (2, 2),   // 7
+        (1, 3),   // 8
+        (0, 3),   // 9
+        (-1, 3),  // 10
+        (-2, 2),  // 11
+        (-3, 1),  // 12
+        (-3, 0),  // 13
+        (-3, -1), // 14
+        (-2, -2), // 15
+        (-1, -3), // 16
+    ];
+
     /// decide whether `pix_x` is within the pixel intensity bounds of `pix_p`:
     /// - `pix_p` is the central pixel
     /// - `pix_x` is one of the neighboring pixels
@@ -79,29 +99,8 @@ impl FASTDetector {
         false
     }
 
-    fn check_pixel(&self, img: &GrayImage, coords: ImgCoords) -> bool {
-        let p = img.get_pixel(coords.x, coords.y)[0];
-
-        let neighbor_coords = [
-            (0, -3),  // 1
-            (1, -3),  // 2
-            (2, -2),  // 3
-            (3, -1),  // 4
-            (3, 0),   // 5
-            (3, 1),   // 6
-            (2, 2),   // 7
-            (1, 3),   // 8
-            (0, 3),   // 9
-            (-1, 3),  // 10
-            (-2, 2),  // 11
-            (-3, 1),  // 12
-            (-3, 0),  // 13
-            (-3, -1), // 14
-            (-2, -2), // 15
-            (-1, -3), // 16
-        ];
-
-        let neighbor_vals: Vec<u8> = neighbor_coords
+    fn neighbor_vals(img: &GrayImage, coords: ImgCoords) -> Vec<u8> {
+        Self::NEIGHBOR_RELATIVE_COORDS
             .iter()
             .map(|n_coords| -> u8 {
                 img.get_pixel(
@@ -109,21 +108,33 @@ impl FASTDetector {
                     (coords.y as i64 + n_coords.1 as i64) as u32,
                 )[0]
             })
-            .collect();
+            .collect()
+    }
 
-        let mut neighbor_tags: Vec<ComparedToCentre> = neighbor_vals
+    fn neighbor_tags(
+        &self,
+        neighbor_vals: &Vec<u8>,
+        central_pixel_val: u8,
+    ) -> Vec<ComparedToCentre> {
+        neighbor_vals
             .iter()
             .map(|n_val| -> ComparedToCentre {
-                if self.is_black(p, *n_val) {
+                if self.is_black(central_pixel_val, *n_val) {
                     ComparedToCentre::Black
-                } else if self.is_white(p, *n_val) {
+                } else if self.is_white(central_pixel_val, *n_val) {
                     ComparedToCentre::White
                 } else {
                     ComparedToCentre::InBounds
                 }
             })
-            .collect();
+            .collect()
+    }
 
+    fn check_pixel(&self, img: &GrayImage, coords: ImgCoords) -> bool {
+        let p = img.get_pixel(coords.x, coords.y)[0];
+        let neighbor_vals = Self::neighbor_vals(img, coords);
+
+        let mut neighbor_tags: Vec<ComparedToCentre> = self.neighbor_tags(&neighbor_vals, p);
         // repeat the first min_contig_neighbors elements so that you can loop until the very last
         // element of the neighbor_tags
         neighbor_tags.extend(neighbor_tags[..self.params.min_contig_neighbors as usize].to_vec());
@@ -132,13 +143,12 @@ impl FASTDetector {
             .extend(neighbor_tags[..self.params.min_contig_neighbors as usize].to_vec());
 
         let corner_masks = [
-            vec![ComparedToCentre::Black; self.params.min_contig_neighbors as usize],
-            vec![ComparedToCentre::White; self.params.min_contig_neighbors as usize],
+            vec![ComparedToCentre::Black; self.params.min_contig_neighbors as usize], // all black
+            vec![ComparedToCentre::White; self.params.min_contig_neighbors as usize], // all white
         ];
 
         // iterate over the neighbor tags - if you find N consecutive Black or N consecutive White
         // pixels then this is going to be a corner
-        // you have to loop over - TODO Use circular buffer here
         for i in 0..neighbor_tags.len() {
             for mask in corner_masks.iter() {
                 if neighbor_tags_extended[i..i + self.params.min_contig_neighbors as usize]
@@ -152,8 +162,68 @@ impl FASTDetector {
         false
     }
 
-    fn non_maximal_suppression(&self, img: &GrayImage, features: &mut Vec<ImgCoords>) {}
-}
+    /// Compute a score for a pixel already identified as a corner
+    /// See Eq. 8 of "Machine learning for high-speed corner detection" paper.
+    fn get_score(&self, img: &GrayImage, coords: ImgCoords) -> u32 {
+        let p = img.get_pixel(coords.x, coords.y)[0];
+        let neighbor_vals = Self::neighbor_vals(img, coords);
+        let neighbor_tags: Vec<ComparedToCentre> = self.neighbor_tags(&neighbor_vals, p);
+
+        let mut sum_black: u32 = 0;
+        let mut sum_white: u32 = 0;
+        for i in 0..neighbor_vals.len() {
+            if neighbor_tags[i] == ComparedToCentre::Black {
+                sum_black += ((neighbor_vals[i] as i16 - p as i16).abs()
+                    - (self.params.threshold as i16)) as u32;
+            } else if neighbor_tags[i] == ComparedToCentre::White {
+                sum_white += ((neighbor_vals[i] as i16 - p as i16).abs()
+                    - (self.params.threshold as i16)) as u32;
+            }
+        }
+
+        cmp::max(sum_black, sum_white)
+    }
+
+    fn nonmax_suppression(&self, img: &GrayImage, features: &mut Vec<ImgCoords>) {
+        let mut indices_to_remove: Vec<usize> = vec![];
+        // TODO probaly not optimal - Rewrite
+        for (idx, feat) in features.iter().enumerate() {
+            // check for neighbors - keep the pixel with the biggest sum of absolute diffs
+            for idx2 in idx + 1..features.len() {
+                // skip if already marked as remove
+                if indices_to_remove.contains(&idx2) {
+                    continue;
+                }
+
+                let feat2 = &features[idx2];
+
+                // neighbors?
+                if (feat2.x as i64 - feat.x as i64).abs() > 1
+                    || (feat2.y as i64 - feat.y as i64).abs() > 1
+                {
+                    continue;
+                }
+
+                // keep pixel with highest score - mark other as removed
+                let score1 = self.get_score(img, *feat);
+                let score2 = self.get_score(img, *feat2);
+
+                // if remove idx1
+                if score1 <= score2 {
+                    indices_to_remove.push(idx);
+                    break;
+                } else {
+                    indices_to_remove.push(idx2);
+                    continue;
+                }
+            }
+        }
+
+        // remove
+        let mut i: usize = 0;
+        features.retain(|_| (!indices_to_remove.contains(&i), i += 1).0);
+    } // nonmax_suppression
+} // impl FASTDetector
 
 impl KeypointDetector for FASTDetector {
     type Params = FASTDetectorParams;
@@ -168,6 +238,7 @@ impl KeypointDetector for FASTDetector {
         &self.params
     }
 
+    // TODO - add a mask argument for applying the detector only at a part of the image
     fn detect(&self, img: &Self::ImageView, features: &mut Vec<ImgCoords>) {
         // iterate over all pixels - ignore first and last 3 rows and columns
         for row in 3..img.height() - 3 {
@@ -185,10 +256,12 @@ impl KeypointDetector for FASTDetector {
                 if self.check_pixel(img, coords) {
                     features.push(coords);
                 }
-
-                // non-maximal suppression
-                self.non_maximal_suppression(img, features);
             }
+        }
+
+        // non-maximal suppression
+        if self.params.do_nonmax_suppression {
+            self.nonmax_suppression(img, features);
         }
     }
 }
@@ -200,11 +273,11 @@ impl KeypointDetector for FASTDetector {
 pub struct FASTDetectorParams {
     /// Intensity threshold (0, 255) for determining whether the intensity of a neighboring pixel
     /// is significantly higher or lower than the central pixel
-    threshold: u8,
+    pub threshold: u8,
     /// Number of neighbors to consider when determining whether a pixel is a corner
-    min_contig_neighbors: u8,
-    do_high_speed_test: bool,
-    do_non_maximal_suppression: bool,
+    pub min_contig_neighbors: u8,
+    pub do_high_speed_test: bool,
+    pub do_nonmax_suppression: bool,
 }
 
 impl Default for FASTDetectorParams {
@@ -213,7 +286,7 @@ impl Default for FASTDetectorParams {
             threshold: 10,
             min_contig_neighbors: 12,
             do_high_speed_test: true,
-            do_non_maximal_suppression: true,
+            do_nonmax_suppression: true,
         }
     }
 }
